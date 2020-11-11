79f717874c73f59e24e5a98f4c4e55c7dd93b7a6
karanlakharwal5 <karanlakharwal@gmail.com>
2020-11-11 23:07:03 +1100

    adding user login and registration functionality

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e046f4794f6e4ec2e90158b9dd5d3adf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from FlaskBlog import routes
